6b043c784083a8fd205a5236f4a1b5758f660615
tciles <thomasciles@gmail.com>
2019-12-01 14:38:38 +0100

    day 1 - clean

--- 1.2/code.js
+++ 1.2/code.js
@@ -1,11 +1,13 @@
-const calcRequiredFuel = (mass, total = 0) => {
-  const requiredFuel = Math.floor(mass/3) - 2;
+const calcRequiredFuel = (mass) => Math.floor(mass/3) - 2;
+
+const calcRequiredFuel2 = (mass, total = 0) => {
+  const requiredFuel = calcRequiredFuel(mass);
 
   if (requiredFuel <= 0) {
     return total;
   }
 
-  return calcRequiredFuel(requiredFuel, total + requiredFuel);
+  return calcRequiredFuel2(requiredFuel, total + requiredFuel);
 }
 
 /**
@@ -17,7 +19,7 @@ function run(input) {
   let total = 0;
   
   input.forEach((line) => {
-    total += calcRequiredFuel(parseInt(line));
+    total += calcRequiredFuel2(parseInt(line));
   });
 
   console.log(total);
